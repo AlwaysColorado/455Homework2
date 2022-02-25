d71df7918a18983d1b4db5bc4bd1ad61ae32c55b
MaximillianWhelan <maximillianwhelan@gmail.com>
2022-02-24 17:04:07 -0700

    Client added method to generateRandomByteMessages

--- src/main/java/cs455/scaling/client/Client.java
+++ src/main/java/cs455/scaling/client/Client.java
@@ -1,4 +1,17 @@
 package cs455.scaling.client;
 
 public class Client {
+
+
+    public Client(){
+        
+    }
+
+     private byte[] generateRandomByteMessage(){
+        Random random = new Random();
+        byte[] byteMessage = new byte[8000];
+        random.nextBytes(byteMessage);
+        return byteMessage;
+    }
+
 }
