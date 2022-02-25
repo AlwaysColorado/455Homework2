9b2ca0a77dcf161b491c41802174c1bd1e61661f
MaximillianWhelan <maximillianwhelan@gmail.com>
2022-02-24 19:13:37 -0700

    Executor class + thoughts about ThreadPool Manager

--- src/main/java/cs455/scaling/threadpool/Executor.java
+++ src/main/java/cs455/scaling/threadpool/Executor.java
@@ -1,5 +1,22 @@
 package cs455.scaling.threadpool;
 
-public class Executor{
+public class Executor implements Runnable{
+    private BlockingQueue<Runnable> blockingQueue;
 
+    public Executor(BlockingQueue<Runnable> bq){
+        this.blockingQueue = bq;
+    }
+
+    @Override
+    public void run(){
+        try{
+            while(true){
+                Runnable executorTask = blockingQueue.remove(); // this will remove a task from the list 
+                executorTask.run(); // run the task 
+                System.out.println("Thread has finised its task"); // message for testing can be removed later
+            }
+        } catch (InterruptedException e){
+            System.out.println("Interrupted Exception at Executor");
+        }
+    }
 }
--- src/main/java/cs455/scaling/threadpool/ThreadPoolManager.java
+++ src/main/java/cs455/scaling/threadpool/ThreadPoolManager.java
@@ -1,5 +1,13 @@
 package cs455.scaling.threadpool;
 
 public class ThreadPoolManager{
+    // tasks to handle: 
 
+    // Client registering 
+
+    // Reading incoming data from a client 
+
+    // Batch has reached batch-size -- get worker thread to do something 
+
+    // Batch has expired batch-time -- get worker thread to do something 
 }
