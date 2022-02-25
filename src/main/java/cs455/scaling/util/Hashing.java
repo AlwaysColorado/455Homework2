de3d1c07376ee708cfee66b4eb604f2066cce2be
MaximillianWhelan <maximillianwhelan@gmail.com>
2022-02-24 17:42:24 -0700

    Added util folder and Hashing class with hashing function + padding

---
+++ src/main/java/cs455/scaling/util/Hashing.java
@@ -0,0 +1,30 @@
+package cs455.scaling.util;
+
+import java.security.MessageDigest;
+import java.lang.StringBuilder;
+
+public class Hashing{
+
+    // empty constructor
+    public Hashing(){
+
+    }
+
+    public String SHA1FromBytes(byte[] data) { 
+        // create digest with SHA1. I think 256 would be be better but need to check specifications 
+        MessageDigest digest = MessageDigest.getInstance("SHA1"); 
+        byte[] hash  = digest.digest(data); 
+        // get the hash number
+        BigInteger hashInt = new BigInteger(1, hash); 
+        // build a string with the hash
+        StringBuilder hashMessage = new StringBuilder(hashInt.toString(16)); 
+        // pad with leading zeros
+            while (hashMessage.length() < 32) 
+            { 
+                hashMessage.insert(0, '0'); 
+            } 
+        // return the tostring with padding 
+        return hashMessage.toString(); 
+} 
+ 
+}
