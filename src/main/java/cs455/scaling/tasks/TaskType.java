d52fa0ea81763d3bfe57043e2c212028902bbb88
Lauren Dziak <sixfobit@colostate.edu>
2022-03-02 14:33:22 -0700

    fourth task type

--- src/main/java/cs455/scaling/tasks/TaskType.java
+++ src/main/java/cs455/scaling/tasks/TaskType.java
@@ -2,5 +2,5 @@ package cs455.scaling.tasks;
 
 //Enum for nice switch cases if we need it.
 public enum TaskType {
-    REGISTER_CLIENT, HANDLE_TRAFFIC, BATCH_DATA
+    REGISTER_CLIENT, HANDLE_TRAFFIC, BATCH_DATA, CLIENT_RESPONSE
 }
