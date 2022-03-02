87d7148e4e099318870e39f0bf9bfda32b8e90f3
Lauren Dziak <sixfobit@colostate.edu>
2022-03-02 14:51:44 -0700

    refactor to use task. TODO: Refactor executeThreadPool()

--- src/main/java/cs455/scaling/threadpool/ThreadPool.java
+++ src/main/java/cs455/scaling/threadpool/ThreadPool.java
@@ -1,29 +1,31 @@
 package cs455.scaling.threadpool;
 
+import cs455.scaling.tasks.Task;
+
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ThreadPool{
     // instantiate blockingQueue 
-    private final BlockingQueue<Runnable> blockingQueue;
-    private final WorkerPool[] workers;
+    private final BlockingQueue<Task> blockingQueue;
+    private final Worker[] workers;
     private AtomicBoolean isKillThreads = new AtomicBoolean(false);
 
     public ThreadPool(int threadPoolSize) {
         // create new blockingQueue
         blockingQueue = new LinkedBlockingQueue<>(); // new BlockingQueue that is unbounded.
-        workers = new WorkerPool[threadPoolSize]; // set the workerPool with threadpool size
+        workers = new Worker[threadPoolSize]; // set the workerPool with threadpool size
         //this.isKillThreads = new AtomicBoolean(false); // set kill-threads method to false
         // this needs to be edited because currently all workers can go, but we want to limit when workers can go
         for(int i=0; i<threadPoolSize; i++){
-            workers[i] = new WorkerPool();
+            workers[i] = new Worker();
             workers[i].start();
         }
 
     }
 
-    public void executeThreadPool(Runnable task){
+    public void executeThreadPool(Task task){
         // we only want to execute the task based on conditions, such as batch-size or batch-time
         synchronized (blockingQueue){
             blockingQueue.add(task);
@@ -36,8 +38,8 @@ public class ThreadPool{
     }
 
 
-    private class WorkerPool extends Thread{
-        private Runnable task;
+    private class Worker extends Thread{
+        private Task task;
         public void run(){
             while(!isKillThreads.get()){
                 synchronized (blockingQueue){
@@ -48,16 +50,16 @@ public class ThreadPool{
                             e.printStackTrace();
                         }
                     }
-                        task = blockingQueue.poll();
-                        try{
-                            assert task != null;
-                            System.out.println("ThreadPool: Thread starting");
-                            task.run(); // run the task
-                            System.out.println("ThreadPool: Thread has finished its task"); // message for testing can be removed later
-                        }
-                        catch (RuntimeException e){
-                            System.out.println("ThreadPool: There was a problem with running the task" + e);
-                        }
+                    task = blockingQueue.poll();
+                    try{
+                        assert task != null;
+                        System.out.println("ThreadPool: Thread starting");
+                        task.executeTask(); // run the task
+                        System.out.println("ThreadPool: Thread has finished its task"); // message for testing can be removed later
+                    }
+                    catch (RuntimeException e){
+                        System.out.println("ThreadPool: There was a problem with running the task" + e);
+                    }
             }
         }
 
