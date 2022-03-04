f47cc82551ff359c90f1b53ad853581f7a5dd3f4
Lauren Dziak <sixfobit@colostate.edu>
2022-03-04 13:48:36 -0700

    redo to be two tasks

--- src/main/java/cs455/scaling/tasks/TaskType.java
+++ src/main/java/cs455/scaling/tasks/TaskType.java
@@ -2,5 +2,5 @@ package cs455.scaling.tasks;
 
 //Enum for nice switch cases if we need it.
 public enum TaskType {
-    REGISTER_CLIENT, HANDLE_TRAFFIC, BATCH_DATA, CLIENT_RESPONSE
+    REGISTER_CLIENT, HANDLE_TRAFFIC
 }
