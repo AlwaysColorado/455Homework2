2f4880a99395313eea93754ad71f657df006b966
MaximillianWhelan <maximillianwhelan@gmail.com>
2022-03-08 13:29:24 +0000

    ThreadPool Refactor.

--- src/main/java/cs455/scaling/threadpool/ThreadPool.java
+++ src/main/java/cs455/scaling/threadpool/ThreadPool.java
@@ -10,7 +10,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ThreadPool{
     // instantiate blockingQueue 
-    private BlockingQueue<Task> taskList;
+    private BlockingQueue<Task> taskList; {
+    };
     private AtomicBoolean isKillThreads = new AtomicBoolean(false);
     private Queue<BlockingQueue<Task>> batchList = new LinkedList<>();
 
@@ -20,7 +21,7 @@ public class ThreadPool{
 
         Worker[] workers = new Worker[threadPoolSize];
 
-        // pull a taskList from batchList
+            // pull a taskList from batchList
         synchronized (batchList) {
             if (batchList.isEmpty()) {
                 batchList.wait();
@@ -30,12 +31,12 @@ public class ThreadPool{
             taskList = batchList.poll();
         }
         // each worker should work on a separate batch
+
         for (int i = 0; i < threadPoolSize; i++) {
             workers[i] = new Worker(); // create a new worker to do all tasks from batch
-            if (workers[i].isAvailable.get()) { // check if there is a free worker
-                workers[i].start(); // run the worker
-            }
+            workers[i].start(); // run the worker
         }
+
     }
 
     public void addTaskList(BlockingQueue<Task> taskList){
@@ -46,40 +47,41 @@ public class ThreadPool{
     }
 
     public void killThreads(){
-        isKillThreads = new AtomicBoolean(true);
+        isKillThreads.set(true);
     }
 
 
     private class Worker extends Thread{
         private Task task;
-        private AtomicBoolean isAvailable;
-        private AtomicBoolean running = new AtomicBoolean(true);
+        private AtomicBoolean isAvailable = new AtomicBoolean(false);
+        private AtomicBoolean running = new AtomicBoolean(false);
 
         private void killIndividualThread(){
-            running = new AtomicBoolean(false);
+            running.set(false);
         }
 
         public void run(){
+            running.set(true);
             while(running.get()){
+                isAvailable.set(false);
                 synchronized (taskList){
                     while (taskList.size() == 0){
                         try{
                             taskList.wait();
-                            isAvailable = new AtomicBoolean(false);
+                            isAvailable.set(true);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
                     task = taskList.poll();
-                    isAvailable = new AtomicBoolean(true);
                     try{
                         assert task != null;
-                        System.out.println("ThreadPool: Thread starting");
+                        System.out.println("Worker Thread: Thread starting");
                         task.executeTask(); // run the task
-                        System.out.println("ThreadPool: Worker has finished a task"); // message for testing can be removed later
+                        System.out.println("Worker Thread: Worker has finished a task"); // message for testing can be removed later
                     }
                     catch (RuntimeException e){
-                        System.out.println("ThreadPool: There was a problem with running the task" + e);
+                        System.out.println("Worker Thread: There was a problem with running the task" + e);
                     }
             }
         }
