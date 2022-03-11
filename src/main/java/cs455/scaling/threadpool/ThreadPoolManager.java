2f28d7a027b40985ed45ceab86775fec3c893b85
Meridith McCann <meridith.mccann@colostate.edu>
2022-03-11 10:53:12 -0700

    Add a few comments and remove the unused imports

--- src/main/java/cs455/scaling/threadpool/ThreadPoolManager.java
+++ src/main/java/cs455/scaling/threadpool/ThreadPoolManager.java
@@ -3,6 +3,7 @@ package cs455.scaling.threadpool;
 import cs455.scaling.tasks.Task;
 
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ThreadPoolManager implements Runnable{
 
@@ -11,23 +12,25 @@ public class ThreadPoolManager implements Runnable{
     private final int batchSize;
     private final long batchTime;
     private long startTime;
-    private boolean tasksToAdd; // Server can set to false to exit run() while loop
+    private AtomicBoolean tasksToAdd; // Server can set to false to exit run() while loop
 
     public ThreadPoolManager (int threadPoolSize, int bs, long bt) {
 
         threadPool = new ThreadPool(threadPoolSize);
+        threadPool.start();
         batchSize = bs;
         batchTime = bt * 1000000000; // SECOND-TO-NANO Conversion
-        tasksToAdd = true;
+        tasksToAdd.set(true);
+        newTaskList();
     }
 
     @Override
     public void run() {
         // While the server is still sending tasks
-        while (tasksToAdd) {
+        while (tasksToAdd.get()) {
             // Create the first taskList
-            if (taskList == null || timesUp() || taskList.size() >= batchSize) {
-                startNewTaskList();
+            if (timesUp() || taskList.size() >= batchSize) {
+                addAndStartNewTaskList();
             }
         }
         // Ensure the last TaskList is added to the ThreadPool for completion
@@ -35,12 +38,14 @@ public class ThreadPoolManager implements Runnable{
     }
 
     // Start a new TaskList
-    private void startNewTaskList() {
+    private void addAndStartNewTaskList() {
         // If this isn't the first TaskList, pass the list to the ThreadPool
-        if (taskList != null) {
-            threadPool.addTaskList(taskList);
-        }
+        threadPool.addTaskList(taskList);
+        newTaskList();
 
+    }
+
+    private void newTaskList() {
         // Create a new taskList and set the start time
         taskList = new LinkedBlockingQueue<>();
         startTime = System.nanoTime();
@@ -63,7 +68,7 @@ public class ThreadPoolManager implements Runnable{
 
             // If batchTime or batchSize have been met
             if (timesUp() || taskList.size() == batchSize) {
-                startNewTaskList();
+                addAndStartNewTaskList();
             }
 
             // Try adding the task
@@ -75,7 +80,7 @@ public class ThreadPoolManager implements Runnable{
     // Run while loop condition to false so the final tasks will be added to the
     //      BatchList before closing the TPM
     public synchronized void terminate() {
-        tasksToAdd = false;
+        tasksToAdd.set(false);
     }
 
 }
