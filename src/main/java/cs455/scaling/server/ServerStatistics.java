94c12bfab0dd74b90b97f37ef64089abf1f6b35d
Meridith McCann <meridith.mccann@colostate.edu>
2022-03-13 13:40:14 -0600

    Per mean client throughput math

--- src/main/java/cs455/scaling/server/ServerStatistics.java
+++ src/main/java/cs455/scaling/server/ServerStatistics.java
@@ -21,14 +21,14 @@ public class ServerStatistics extends TimerTask {
         // Retrieve the HashMap with the stats
         clientStatistics = server.getClientStatistics();
         List<Integer> msgCounts = clientBreakdown();
+        long totalMsgCt = getTotalMsgCount(msgCounts); // total messages sent in the last 20 seconds
 
         // DO THE MATH!
         // ------------
         int activeClientConnections = msgCounts.size(); // Number of active client connections (in last 20 seconds)
-        long totalMsgCt = getTotalMsgCount(msgCounts); // total messages sent in the last 20 seconds
         double throughput = getThroughput(totalMsgCt); // Average number of messages processed per second in the last 20 seconds
-        double meanPerClientThroughput = -99; // TODO: do the math
-        double sdPerClientThroughput = -99; //TODO: do the math
+        double meanPerClientThroughput = getMeanPerClientThroughput(msgCounts, activeClientConnections); // Mean of the per client throughput
+        double sdPerClientThroughput = getStdDevPerClientThroughput(msgCounts, meanPerClientThroughput, activeClientConnections); // Standard Deviation of the per client throughput
 
         // TODO: Format floating point numbers to limit length
         System.out.printf("[%s] Server Throughput: %f messages/s, Active Client Connections: %d, " +
@@ -36,12 +36,13 @@ public class ServerStatistics extends TimerTask {
                 date, throughput, activeClientConnections, meanPerClientThroughput, sdPerClientThroughput);
     }
 
+
     private List<Integer> clientBreakdown() {
         int clientCount = 0;
 
         // Increment client count if value > 0
         List<Integer> messageCounts = (List<Integer>) clientStatistics.values(); // Get the values
-        messageCounts.removeIf(v -> v == 0); // Remove 'zero' entries TODO: Is this written correctly?
+        messageCounts.removeIf(v -> v == 0); // Remove 'zero' entries
 
         return messageCounts;
     }
@@ -55,7 +56,21 @@ public class ServerStatistics extends TimerTask {
     }
 
     private double getThroughput(long totalMsgCount) {
-
         return (totalMsgCount / 20F);
     }
+
+    private double getMeanPerClientThroughput(List<Integer> perClientCounts, int clients) {
+
+        double sum = 0.0;
+        for (int count : perClientCounts) {
+            sum += count;
+        }
+        return (sum / clients);
+
+    }
+
+    public double getStdDevPerClientThroughput(List<Integer> perClientCounts, double meanPerClientThroughput, int clients) {
+
+    }
+
 }
