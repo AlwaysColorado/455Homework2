fd939fc2d6cc839ac5c31594cc6cd2fa29d4e048
Meridith McCann <meridith.mccann@colostate.edu>
2022-03-15 11:36:10 -0600

    Put Hashtable values in an array rather than using .values() in ServerStats.
    .values() doesn't give a flattened copy anyway.

--- src/main/java/cs455/scaling/server/ServerStatistics.java
+++ src/main/java/cs455/scaling/server/ServerStatistics.java
@@ -19,9 +19,9 @@ public class ServerStatistics extends TimerTask {
         String date = new Date().toString(); // Get timestamp
 
         clientStatistics = server.getClientStatistics(); // Retrieve the HashMap with the stats
-        List<Integer> msgCounts = (List<Integer>) clientStatistics.values(); // Get values from Hashtable
+        int[] msgCounts = getCounts(); // Get values from Hashtable
 
-        int activeClientConnections = msgCounts.size(); // Number of client connections
+        int activeClientConnections = msgCounts.length; // Number of client connections
         double throughput = getThroughput(msgCounts); // Average number of messages processed per second in the last 20 seconds
         double meanPerClientThroughput = getMeanPerClientThroughput(msgCounts, activeClientConnections); // Mean of the per client throughput
         double sdPerClientThroughput = getStdDevPerClientThroughput(msgCounts, meanPerClientThroughput, activeClientConnections); // Standard Deviation of the per client throughput
@@ -32,16 +32,26 @@ public class ServerStatistics extends TimerTask {
                 date, throughput, activeClientConnections, meanPerClientThroughput, sdPerClientThroughput);
     }
 
+    private int[] getCounts() {
+        int[] counts = new int[clientStatistics.size()];
 
-    private double getThroughput(List<Integer> msgCounts) {
+        int i = 0;
+        for (Integer count : clientStatistics.values()) {
+            counts[i] = count;
+            i++;
+        }
+        return counts;
+    }
+
+    private double getThroughput(int[] msgCounts) {
         long totalMsgCount = 0; // total messages sent in the last 20 seconds
-        for (Integer count : msgCounts) {
+        for (int count : msgCounts) {
             totalMsgCount += count;
         }
         return (totalMsgCount / 20F); // Messages sent per second
     }
 
-    private double getMeanPerClientThroughput(List<Integer> perClientCounts, int clients) {
+    private double getMeanPerClientThroughput(int[] msgCounts, int clients) {
 
         // If no active clients
         if (clients == 0) {
@@ -49,15 +59,15 @@ public class ServerStatistics extends TimerTask {
         }
 
         // else...
-        double sum = 0.0;
-        for (int count : perClientCounts) {
-            sum += count;
+        double totalMsgCount = 0.0;
+        for (int count : msgCounts) {
+            totalMsgCount += count;
         }
-        return (sum / clients);
+        return (totalMsgCount / clients);
 
     }
 
-    public double getStdDevPerClientThroughput(List<Integer> counts, double mean, int clients) {
+    public double getStdDevPerClientThroughput(int[] msgCounts, double mean, int clients) {
         // If no active clients
         if (clients == 0) {
             return 0.0;
@@ -65,7 +75,7 @@ public class ServerStatistics extends TimerTask {
 
         // else...
         double stdDev = 0.0;
-        for (int count : counts) {
+        for (int count : msgCounts) {
             stdDev += Math.pow(count - mean, 2);
         }
         return Math.sqrt( stdDev / clients);
