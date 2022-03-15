764a53cb30965beded986994da9bcb958b0afe73
Lauren Dziak <sixfobit@colostate.edu>
2022-03-14 18:46:38 -0600

    Register client to Server's hashtable.

--- src/main/java/cs455/scaling/tasks/REGISTER_CLIENT.java
+++ src/main/java/cs455/scaling/tasks/REGISTER_CLIENT.java
@@ -1,5 +1,7 @@
 package cs455.scaling.tasks;
 
+import cs455.scaling.server.Server;
+
 import java.io.IOException;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
@@ -11,11 +13,13 @@ public class REGISTER_CLIENT extends Task{
     //References to the server's selector and the client's socket.
     public Selector selector;
     public SocketChannel clientSocket;
+    private Server parent;
 
-    public REGISTER_CLIENT(Selector selector, SocketChannel clientSocket) {
+    public REGISTER_CLIENT(Selector selector, SocketChannel clientSocket, Server parent) {
         super(TaskType.REGISTER_CLIENT);
         this.selector = selector;
         this.clientSocket = clientSocket;
+        this.parent = parent;
     }
 
     @Override
@@ -23,6 +27,7 @@ public class REGISTER_CLIENT extends Task{
         try {
             clientSocket.configureBlocking(false);
             clientSocket.register(selector, SelectionKey.OP_READ);
+            parent.registerOneClient(clientSocket.getLocalAddress());
         } catch (ClosedChannelException e) {
             //if the client socket is closed, the server should move on. Do nothing.
             //e.printStackTrace();
