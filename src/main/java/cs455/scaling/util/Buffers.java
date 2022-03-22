583c499c788d5fb91492b413323618564a89b823
Lauren Dziak <sixfobit@colostate.edu>
2022-03-22 15:57:19 -0600

    final

--- src/main/java/cs455/scaling/util/Buffers.java
+++ src/main/java/cs455/scaling/util/Buffers.java
@@ -3,6 +3,6 @@ package cs455.scaling.util;
 import java.nio.ByteBuffer;
 
 public class Buffers {
-    public ByteBuffer writeBuffer = ByteBuffer.allocate(40);
-    public ByteBuffer readBuffer = ByteBuffer.allocate(8196);
+    public final ByteBuffer writeBuffer = ByteBuffer.allocate(40);
+    public final ByteBuffer readBuffer = ByteBuffer.allocate(8196);
 }
